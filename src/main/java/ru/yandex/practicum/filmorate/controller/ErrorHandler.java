f4af79626a22dfb3049f943b5d42176a6532690b
somikaaaA <s0mik.a@yandex.ru>
2024-11-16 15:44:19 +0300

    check API

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -9,20 +9,58 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.*;
 
+import java.util.Objects;
+
 @RestControllerAdvice("ru.yandex.practicum.filmorate")
 @Slf4j
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND) //404
-    public ErrorResponse handleNotFound(NotFoundException e) {
-        log.error("Not found {}", e.getMessage());
+    public ErrorResponse handleParameterNotValid(ValidationException e) {
+        log.error("Validation exception {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST) //400
-    public ErrorResponse handleValidationException(ValidationException e) {
+    public ErrorResponse handleBadRequest(BadRequestException e) {
         log.error("Validation exception {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
+    public ErrorResponse handleAnnotations(MethodArgumentNotValidException e) {
+        String response = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
+        log.error("Incorrect argument {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND) //404
+    public ErrorResponse handleNotFound(NotFoundException e) {
+        log.error("Incorrect argument {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND) //404
+    public ErrorResponse baseNotFound(DataAccessException e) {
+        log.error("Database error {}" + e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
+    public ErrorResponse dataBaseNotUpdate(InternalServerException e) {
+        log.error("Database update exception {}" + e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.OK) //200
+    public ErrorResponse friendNotFound(NotFriendException e) {
+        log.error("Friends not found");
         return new ErrorResponse(e.getMessage());
     }
 
@@ -31,33 +69,5 @@ public class ErrorHandler {
     public ErrorResponse handleAllExceptions(Exception e) {
         log.error("Unexpected error occurred", e);
         return new ErrorResponse("An unexpected error occurred");
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
-    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-        log.error("Validation exception {}", e.getMessage());
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND) //404
-    public ErrorResponse dataBaseNotFound(DataAccessException e) {
-        log.error("Data base is not found {}" + e.getMessage());
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
-    public ErrorResponse dataBaseNotUpdate(InternalServerException e) {
-        log.error("Update exception {}" + e.getMessage());
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.OK) //200
-    public ErrorResponse friendNotFound(NotFriendException e) {
-        log.error("Friend is not found");
-        return new ErrorResponse(e.getMessage());
     }
 }
