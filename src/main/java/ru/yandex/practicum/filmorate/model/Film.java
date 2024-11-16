7fb8ba4175ae714d2b51d89a20e93ad1f7cae19d
somikaaaA <s0mik.a@yandex.ru>
2024-11-16 17:09:28 +0300

    Добавлена проверка валидации даты релиза

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
